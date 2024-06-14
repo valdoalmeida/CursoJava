99546aeb460a948957557ee58b64e274d1547b53
valdo <valdo@Home_PC>
2024-06-14 08:53:08 -0300

    teste

---
+++ cursoJava/src/javaConstrutor/javaConstrutor.java
@@ -0,0 +1,26 @@
+package javaConstrutor;
+
+public class javaConstrutor {
+
+	String tipo;
+	String marca;
+	String cor;
+	int ano;
+	float valor;
+	boolean vendido;
+	
+	
+	public javaConstrutor(String tipo, String marca, String cor, int ano, float valor, boolean vendido) {
+		
+		this.tipo = tipo;
+		this.marca = marca;
+		this.cor = cor;
+		this.ano = ano;
+		this.valor = valor;
+		this.vendido = vendido;
+		
+		
+		
+	}
+
+}
---
+++ cursoJava/src/javaPrincipal/cursoJavaAula1.java
@@ -0,0 +1,18 @@
+package javaPrincipal;
+
+import javaConstrutor.javaConstrutor;
+
+public class cursoJavaAula1 {
+
+	public static void main(String[] args) {
+
+		String nome = "Valdo Almeida";
+
+		System.out.println(nome);
+
+	}
+	
+	javaConstrutor veiculo1 = new javaConstrutor();
+	javaConstrutor veiculo2 = new javaConstrutor();
+
+}
